d2954f19615aebfb85c873027a43b77bfb04b704
Matthew Willox <skintilla@gmail.com>
2017-04-23 01:30:09 -0600

    added a filter

---
+++ src/Filter/Filter.js
@@ -0,0 +1,88 @@
+export default class Filter {
+	constructor(context, type ="lowpass", cutoff = 1000, resonance = 0.1) {
+		this.context = context;
+		this.destination = this.context.createBiquadFilter();
+		this.type = type;
+		this.cutoff = cutoff;
+		this.resonance = 0.1;
+		this.envelopeAmount = 1;
+		this.envelope = {
+			a: 0,
+			d: 0.5,
+			s: this.cutoff,
+			r: 0.5
+		};
+	}
+
+	on(MidiEvent) {
+		this.start(this.context.currentTime, MidiEvent.frequency);
+	}
+
+	off() {
+		return this.stop(this.context.currentTime);
+	}
+
+	set type(value) {
+		this.destination.type = value;
+	}
+	get type () {
+		return this.destination.type;
+	}
+
+	set cutoff(value) {
+		this.destination.frequency.value = value;
+	}
+	get cutoff () {
+		return this.destination.frequency.value;
+	}
+	set Q (value) {
+		this.destination.Q.value = value;
+	}
+	get Q () {
+		return this.destination.Q.value;
+	}
+
+	start(time) {
+		return this.destination.frequency.setTargetAtTime(this.sustain, time + this.attack, this.decay + 0.001);
+	}
+
+	stop(time) {
+		return this.destination.frequency.setTargetAtTime(this.cutoff, time, this.release);
+	}
+
+	set attack(value) {
+		this.envelope.a = value;
+	}
+
+	get attack() {
+		return this.envelope.a;
+	}
+
+	set decay(value) {
+		this.envelope.d = value;
+	}
+
+	get decay() {
+		return this.envelope.d;
+	}
+
+	set sustain(value) {
+		this.cutoff = value;
+	}
+
+	get sustain() {
+		return this.cutoff;
+	}
+
+	set release(value) {
+		this.envelope.r = value;
+	}
+
+	get release() {
+		return this.envelope.r;
+	}
+
+	connect(destination) {
+		this.destination.connect(destination);
+	}
+}
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import MizzyDevice from "./MizzyDevice";
 import Mizzy from "mizzy";
 import Voice from "./Voices/Voice";
-
+import Filter from "./Filter/Filter";
 
 export default class Vincent extends MizzyDevice {
 
@@ -9,15 +9,16 @@ export default class Vincent extends MizzyDevice {
 			super();
 
 			this.context = new (window.AudioContext || window.webkitAudioContext)();
-			this.merge = this.context.createChannelMerger(8);
-			//this.merge.connect();
+			this.filter = new Filter(this.context);
+			this.filter.connect(this.context.destination);
+			this.oscillatorType = "sawtooth";
 			this.voices = [];
 
 		}
 
 		NoteOn(MidiEvent) {
-			let voice = new Voice(this.context);
-			voice.connect(this.context.destination);
+			let voice = new Voice(this.context, this.oscillatorType);
+			voice.connect(this.filter.destination);
 			voice.on(MidiEvent);
 			this.voices.push(voice);
 		}
@@ -36,10 +37,28 @@ export default class Vincent extends MizzyDevice {
 var vincent = new Vincent();
 
 var m = new Mizzy();
-m.initialize();
-m.bindKeyboard();
-m.keyToggle((e)=>{
-	vincent.NoteOn(e);
-}, (e)=>{
-	vincent.NoteOff(e);
-})
+m.initialize().then(()=> {
+	m.bindToAllInputs();
+	m.bindKeyboard();
+	m.keyToggle((e)=>{
+		vincent.NoteOn(e);
+	}, (e)=>{
+		vincent.NoteOff(e);
+	});
+
+	m.onCC(1, (e) => updateCCValues(e));
+});
+
+
+window.addEventListener("mousemove", (e)=> {
+	var x = Math.round((e.pageX / window.innerWidth) * 127);
+	var y = Math.round((e.pageY / window.innerHeight) * 127);
+	var xmessage = Mizzy.Generate.CCEvent(1, x);
+	m.sendMidiMessage(xmessage);
+	var ymessage = Mizzy.Generate.CCEvent(2, y);
+	m.sendMidiMessage(ymessage);
+});
+
+function updateCCValues(e) {
+	vincent.filter.cutoff = 100 + (e.ratio * 8000);
+}

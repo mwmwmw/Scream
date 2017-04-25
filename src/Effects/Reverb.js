088a115bdae28792db3b0a769c0b21cd8e0d8217
Matthew Willox <matthew@gskinner.com>
2017-04-24 23:10:42 -0600

    Updated the Reverb to use the noise instead of the oscillator

--- src/Effects/Reverb.js
+++ src/Effects/Reverb.js
@@ -1,4 +1,4 @@
-import PercussionVoice from "../Voices/PercussionVoice";
+import Noise from "../Voices/Noise";
 
 export default class Reverb {
 	constructor (context) {
@@ -7,20 +7,20 @@ export default class Reverb {
 		this.reverbTime = 1;
 		this.tailContext = new OfflineAudioContext(2, 48000 * this.reverbTime, 48000);
 		this.buffer = this.tailContext.createBufferSource();
-		this.tail = new PercussionVoice(this.tailContext, "sawtooth");
+		this.tail = new Noise(this.tailContext);
 		this.tail.connect(this.tailContext.destination);
 		this.tail.trigger(100);
-
+		this.tail.off();
 		this.tailContext.startRendering().then((buffer) => {
 
 			this.destination.buffer = buffer;
-			var source = new AudioBufferSourceNode(context, {
+			var source = new AudioBufferSourceNode(this.context, {
 				buffer: buffer
 			});
 			source.start();
 			source.connect(this.context.destination);
 			console.log(source, buffer.getChannelData(0), buffer.getChannelData(1));
-			this.tail.off();
+
 		});
 	}
 

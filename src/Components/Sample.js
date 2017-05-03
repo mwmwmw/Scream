1d913acebac2900791667c1506349a7453da1743
Matthew Willox <skintilla@gmail.com>
2017-05-03 11:33:04 -0600

    way better sample handling

--- src/Components/Sample.js
+++ src/Components/Sample.js
@@ -4,8 +4,9 @@ export default class Sample {
 	constructor (context) {
 
 		this.context = context;
-		this.buffer = this.context.createBuffer(2, this.context.sampleRate * 2, this.context.sampleRate);
-		this.stream = null
+		this.buffer = this.context.createBuffer(2, 1, this.context.sampleRate);
+		this.stream = null;
+		this._recordProcessor = null;
 	}
 
 	load (path) {
@@ -19,33 +20,37 @@ export default class Sample {
 			})
 	}
 
-	record (length = 2000) {
-
+	record (length = null) {
 		this.buffered = 0;
-
-		this.stream = new Float32Array(this.context.sampleRate * (length / 1000));
-
+		this.stream = new Float32Array(0);
 		navigator.mediaDevices.getUserMedia({audio: true, video: false})
 			.then((stream) => {
-
 				let input = this.context.createMediaStreamSource(stream);
-				let processor = this.context.createScriptProcessor(SAMPLE_BUFFER_SIZE, 1, 2);
+				this._recordProcessor = this.context.createScriptProcessor(SAMPLE_BUFFER_SIZE, 1, 2);
 
-				input.connect(processor);
-				processor.connect(this.context.destination);
-				processor.onaudioprocess = (e) => {
+				input.connect(this._recordProcessor);
+				this._recordProcessor.connect(this.context.destination);
+				this._recordProcessor.onaudioprocess = (e) => {
 					let chunk = e.inputBuffer.getChannelData(0);
-					if (this.stream.length - chunk.length * this.buffered > chunk.length ) {
-						this.stream.set(chunk, chunk.length * this.buffered);
-						this.buffered++;
-					}
+					var newStream = new Float32Array(this.stream.length + chunk.length);
+						newStream.set(this.stream);
+						newStream.set(chunk,chunk.length * this.buffered);
+						this.stream = newStream;
+					this.buffered++;
 				};
-				setTimeout(() => {
-					processor.disconnect();
-					this.buffer.copyToChannel(this.stream, 0);
-					this.buffer.copyToChannel(this.stream, 1);
-				}, length);
+				if(length != null) {
+					setTimeout(() => this.stopRecording(), length);
+				}
 			})
+	}
+	stopRecording() {
+		this._recordProcessor.disconnect();
+		this.buffer = this.context.createBuffer(2, this.stream.length, this.context.sampleRate);
+		this.buffer.copyToChannel(this.stream, 0);
+		this.buffer.copyToChannel(this.stream, 1);
+	}
+
+	recordInput (input) {
 
 	}
 

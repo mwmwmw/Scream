3a81b62509dd9beacbe2b6388e2f092f967b8998
Matthew Willox <skintilla@gmail.com>
2017-05-08 17:24:27 -0600

    Import synths correctly

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,7 @@
 import {FilterEnvelope, AmpEnvelope, Sample} from "./Components/Components";
 import {Chorus, Delay, Filter, Reverb, FFT} from "./Effects/Effects";
 import {ComplexVoice, Noise, SamplePlayer, Voice} from "./Voices/Voices";
-import {default as VSS30} from "./VSS30";
-import {default as Vincent} from "./Vincent";
+import {VSS30, Vincent} from "./Synths/Synths";
 
 export const Components = {FilterEnvelope, AmpEnvelope, Sample};
 export const Effects = {Chorus, Delay, Filter, Reverb, FFT};
